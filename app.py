f680a81054076b016fc4b1b87f5b83a8f24afcdd
star327 <135664313+star327@users.noreply.github.com>
2024-12-05 11:00:06 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
